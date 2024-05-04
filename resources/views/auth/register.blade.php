e32866d6bacfd082d5fcc988f2a7f67822f19467
Victor Gabriel <88253070+VictorBritto@users.noreply.github.com>
2024-05-04 17:40:34 -0300

    Ajustes na responsividade
    Foi feito alguns ajustes na responsividade do site, principalmente na tela de produto e na tela de login.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,40 +2,39 @@
 
 @section("content")
 <div class="containercadastro" id="autenticacao">
+    <div class="register-image" id="autenticacao">
+            <img src="{{Vite::asset("resources/img/Mobile login-pana.png")}}" alt="" />
+    </div>
         <div class="formcadastro" id="autenticacao">
             <form action="{{ route("registerAction"); }}" method="POST">
                 @csrf
                 <div class="form-header-cadastro" id="autenticacao">
                     <div class="title">
-                        <h1>Cadastre-se</h1>
+                        <h1>Registre-se</h1>
                     </div>
                 </div>
-
+                <p>Ao criar um contrato na Wattsweb você terá acesso a nossa plataforma. Estamos torcendo pelo seu sucesso ;D</p>
                 <div class="input-group">
                     <div class="input-box">
-                        <label for="name">Nome</label>
                         <input id="name" type="text" name="name" placeholder="Nome" required>
                     </div>
 
                     <div class="input-box">
-                        <label for="email">Email</label>
                         <input id="email" type="email" name="email" placeholder="Digite seu email" required>
                     </div>
 
                     <div class="input-box">
-                        <label for="password">Senha</label>
                         <input id="password" type="password" name="password" placeholder="Digite sua senha" required>
                     </div>
 
                     <div class="input-box">
-                        <label for="passwordConfirm">Repita a Senha</label>
                         <input id="passwordConfirm" type="password" name="passwordConfirm" placeholder="Repita a sua senha" required>
                     </div>
                 </div>
                 
 
-                <div class="continue-button" id="autenticacao">
-                    <button>Cadastrar</button>
+                <div class="register-button" id="autenticacao">
+                    <button>Registre-se</button>
                 </div>
             </form>
         </div>
